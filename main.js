d9268d5a3cce6db56c7f8a303aab02d2ddf50def
MEZEMATE-Hichem <hichem.mezemate@gmail.com>
2022-03-02 19:43:30 +0100

    myWebsite

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let myBtn = document.querySelector('#menuBtn');
+let Box = document.querySelector('#slideBox');
+
+// myBtn.addEventListener('click', showTab);
+
+function showTab() {
+    Box.classList.toggle("change");
+}
+
+
+
+
