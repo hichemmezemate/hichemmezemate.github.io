fa415eb406a66e33154a877096af54fdebed6707
hichemmezemate <78790205+hichemmezemate@users.noreply.github.com>
2022-03-03 13:34:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 let myBtn = document.querySelector('#menuBtn');
 let Box = document.querySelector('#slideBox');
 
-// myBtn.addEventListener('click', showTab);
+myBtn.addEventListener('click', showTab);
+Box.addEventListener('mouseleave', hideTab );
 
 function showTab() {
     Box.classList.toggle("change");
 }
 
-
-
+function hideTab() {
+    Box.classList.toggle('change')
+}
 
